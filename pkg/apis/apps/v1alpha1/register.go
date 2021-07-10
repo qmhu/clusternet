beb980e9fba3524570a3699a9bb438c7cf7870ba
Di Xu <stephenhsu90@gmail.com>
2021-07-10 21:44:37 +0800

    add Manifest to apps/v1alpha1

--- pkg/apis/apps/v1alpha1/register.go
+++ pkg/apis/apps/v1alpha1/register.go
@@ -63,6 +63,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&BaseList{},
 		&Localization{},
 		&Localization{},
+		&Manifest{},
+		&ManifestList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
